ab5442777445581a407e0f3cd8a4c6747cfd932c
Gokul krishna T U <118456500+GokulkrishnaTU@users.noreply.github.com>
2024-03-25 16:17:45 +0600

    Update index.js

--- CM-Backend/index.js
+++ CM-Backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const url = process.env.MONGODB_URI 
+const url = process.env.URL
 console.log(url);
 
 // MongoDB Connection
